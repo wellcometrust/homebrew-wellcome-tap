35898ebd9178222e91f490f3d0edd6eb668f50f9
ivyleavedtoadflax <matt@muanalytics.co.uk>
2020-03-06 11:10:33 -0300

    Bump to 0.4
    Point to https://github.com/wellcometrust/remote/commit/a51eef8cec1734c6eae2e24388f30f32cbda51a7
    @nsorros  How did you calculate the sha256?

--- remote.rb
+++ remote.rb
@@ -1,8 +1,8 @@
 class Remote < Formula
   desc "CLI to connect to AWS instances"
   homepage "https://github.com/wellcometrust/remote/"
-  url "https://github.com/wellcometrust/remote/tarball/46a37ab19ab65836c262fec92e9ddb0658322760"
-  version "0.3"
+  url "https://github.com/wellcometrust/remote/tarball/a51eef8cec1734c6eae2e24388f30f32cbda51a7"
+  version "0.4"
   sha256 "027a2a7ea5fabc01e47641638d729581903d1dbbdc6ab7fe52b00be78c836661"
 
   depends_on "awscli"
