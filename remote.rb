0caf8a2d155f7921cdca97e17a343b8d9ea0d7cb
Matthew Upson <matthew.a.upson@gmail.com>
2020-04-07 18:35:20 -0400

    fix: remote url

--- remote.rb
+++ remote.rb
@@ -1,7 +1,7 @@
 class Remote < Formula
   desc "CLI to connect to AWS instances"
   homepage "https://github.com/wellcometrust/remote/"
-  url "https://github.com/wellcometrust/remote/commit/980a64cd4c59af1fc8b0fc9a19595d46526b7ab5"
+  url "https://github.com/wellcometrust/remote/tarball/980a64cd4c59af1fc8b0fc9a19595d46526b7ab5"
   version "0.6"
   sha256 "f2d940414cd5070471a57c220b900435676cf9a6fb2c7675d751082f27b44a64"
 
