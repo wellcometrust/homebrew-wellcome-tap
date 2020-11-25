d207b0b5c5fa7a73e7116bc9967ec238ae1a2327
Nick Sorros <nsorros@gmail.com>
2020-11-25 12:10:45 +0200

    Update to latest commit of remote

--- remote.rb
+++ remote.rb
@@ -1,9 +1,9 @@
 class Remote < Formula
   desc "CLI to connect to AWS instances"
   homepage "https://github.com/wellcometrust/remote/"
-  url "https://github.com/wellcometrust/remote/tarball/ef47347728bb457d33b19ba5001a83e733669efd"
+  url "https://github.com/wellcometrust/remote/tarball/6f5bd9bbb64c3b0dec40f747d527f88ab03792af"
   version "0.8"
-  sha256 "f03b04606a676c28753d77bba33cbcee71d2799f72126312187634cbfb4270fa"
+  sha256 "88c6a33cdcb913c8646fa369b244c61fee18e70f2176accfb4de403a64661b69"
 
   depends_on "awscli"
   
