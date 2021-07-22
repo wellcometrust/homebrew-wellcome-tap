e5bb7cf32cef979a2d528c43af523fb6e76ece1f
Nick Sorros <nsorros@gmail.com>
2021-07-22 15:11:33 +0300

    Update formula to 0.10

--- remote.rb
+++ remote.rb
@@ -1,9 +1,9 @@
 class Remote < Formula
   desc "CLI to connect to AWS instances"
   homepage "https://github.com/wellcometrust/remote/"
-  url "https://github.com/wellcometrust/remote/tarball/c14c20ca9785d11ee62a0b715b76e75b820ad909"
-  version "0.9"
-  sha256 "a1953003f420a51f752a3e60bc309fa0c273736f1a0d670f960c0b731c2f30d3"
+  url "https://github.com/wellcometrust/remote/tarball/ef25a68953a1cbb1f6ac54cc29e9957a9f4e822b"
+  version "0.10"
+  sha256 "588137b9aa004fa3027dbb9f234541171bcb45ccc50e30ada99dd363e630084c"
 
   depends_on "awscli"
   depends_on "jq"
