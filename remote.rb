d8bc34c9f1fa9ac5ece8498215373a45d76164f4
Nick Sorros <nsorros@gmail.com>
2021-07-22 15:26:58 +0300

    Fix shasum

--- remote.rb
+++ remote.rb
@@ -3,7 +3,7 @@ class Remote < Formula
   homepage "https://github.com/wellcometrust/remote/"
   url "https://github.com/wellcometrust/remote/tarball/ef25a68953a1cbb1f6ac54cc29e9957a9f4e822b"
   version "0.10"
-  sha256 "588137b9aa004fa3027dbb9f234541171bcb45ccc50e30ada99dd363e630084c"
+  sha256 "d9b2b0b912193347438b06b31f27be608a27bcea1448995494c191ab55681064"
 
   depends_on "awscli"
   depends_on "jq"
